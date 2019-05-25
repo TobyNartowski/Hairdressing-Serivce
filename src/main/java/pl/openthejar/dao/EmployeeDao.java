b88806ce5c71d84f737ce9d23031f727b1c62efc
Tobiasz Nartowski <tobiahao@gmail.com>
2019-05-25 15:50:07 +0200

    some major tweaks after some minor tweaks

--- src/main/java/pl/openthejar/dao/EmployeeDao.java
+++ src/main/java/pl/openthejar/dao/EmployeeDao.java
@@ -14,7 +14,7 @@ public class EmployeeDao extends EntityDao<Employee> {
         super(Employee.class);
     }
 
-    public List<Reservation> getAllReservations(Employee employee) {
+    public List<Reservation> getTodaysReservations(Employee employee) {
         final String query = "SELECT r FROM Reservation r";
         TypedQuery<Reservation> typedQuery = entityManager.createQuery(query, Reservation.class);
 
@@ -24,6 +24,22 @@ public class EmployeeDao extends EntityDao<Employee> {
                     && DateUtils.isToday(reservation.getWorkDate().getDate())
                     && reservation.getWorkDate().getEmployees().contains(employee)) {
                 result.add(reservation);
+            }
+        }
+
+        return result;
+    }
+
+    public List<Reservation> getReservations(Employee employee, Boolean done) {
+        final String query = "SELECT r FROM Reservation r";
+        TypedQuery<Reservation> typedQuery = entityManager.createQuery(query, Reservation.class);
+
+        List<Reservation> result = new ArrayList<>();
+        for (Reservation reservation : typedQuery.getResultList()) {
+            if (reservation.getWorkDate() != null && reservation.getWorkDate().getEmployees().contains(employee)) {
+                if ((done == null) || (done == reservation.isDone())) {
+                    result.add(reservation);
+                }
             }
         }
 
--- src/main/java/pl/openthejar/resource/EmployeeResource.java
+++ src/main/java/pl/openthejar/resource/EmployeeResource.java
@@ -22,6 +22,17 @@ public class EmployeeResource {
         return dao.findAll();
     }
 
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOne(@PathParam("id") Long id) {
+        try {
+            return Response.ok(dao.get(id), MediaType.APPLICATION_JSON_TYPE).build();
+        } catch (NoResultException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -30,12 +41,24 @@ public class EmployeeResource {
     }
 
     @GET
-    @Path("/{id}/reservations")
+    @Path("/{id}/todays-reservations")
     @Produces(MediaType.APPLICATION_JSON)
     public Response todaysReservations(@PathParam("id") Long id) {
         try {
             Employee employee = dao.get(id);
-            return Response.ok(dao.getAllReservations(employee), MediaType.APPLICATION_JSON_TYPE).build();
+            return Response.ok(dao.getTodaysReservations(employee), MediaType.APPLICATION_JSON_TYPE).build();
+        } catch (NoResultException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @GET
+    @Path("/{id}/reservations")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllReservations(@PathParam("id") Long id, @QueryParam("done") Boolean done) {
+        try {
+            Employee employee = dao.get(id);
+            return Response.ok(dao.getReservations(employee, done), MediaType.APPLICATION_JSON_TYPE).build();
         } catch (NoResultException e) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
